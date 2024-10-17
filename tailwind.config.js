603558e95bdf1ba75d83c733989447e334c70603
Stif <160525543+StefanTrajkovskii@users.noreply.github.com>
2024-10-17 19:26:07 +0200

    fixed the tailwind problem

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './resources/views/**/*.blade.php',  // Your Blade files
+      './resources/js/**/*.js',            // Your JS files (if needed)
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
